071a1d49e091d9fef908bd0f91a2cc45b6669ce2
Jenny Munoz <juanita027@gmail.com>
2017-02-03 17:26:04 -0800

    Project

---
+++ R Scripts/Rscript_Foraging.R
@@ -0,0 +1,32 @@
+###############################JENNY MUNOZ#####################################
+###############################################################################
+###########Foraging ecology of residents and migrants##########################
+
+#Assumptions": 
+
+# Install: 
+install.packages("car")
+install.packages("vegan")
+install.packages("AICcmodavg")
+
+# Loading packages --------------------------------------------------------
+##general packages  and visualization
+library(lattice)
+library(ggplot2)
+library(car)
+library(visreg)
+
+#########################################################################################################
+
+
+
+
+
+fusca<-read.csv(file.choose("fusca.csv"), stringsAsFactors=FALSE, strip.white=TRUE, na.strings=c("NA",""))
+head(fusca)
+str(fusca)
+
+fusca$Flock.size.Species.number.<- as.numeric(fusca$Flock.size.Species.number.)
+fusca$Flock.size.Birds.number.<- as.numeric(fusca$Flock.size.Birds.number.)  
+fusca$Seasonality<- as.character(fusca$Seasonality)   
+str(fusca)   
